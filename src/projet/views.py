0282f6b303b2b40593dcf0c6a810b655c90b5eec
jedeon <jedeondev@protonmail.com>
2021-04-29 11:57:53 +0200

    test

---
+++ src/projet/views.py
@@ -0,0 +1,23 @@
+#from django.http import HttpResponse
+from django.shortcuts import render 
+     
+
+
+def Accueil(request):
+    return render(request, "accueil.html")
+
+
+
+def Produit(request):
+    return render(request, "produit.html")
+
+
+
+def Contact(request):
+    return render(request, "contact.html")
+
+
+
+def Blog(request):
+    return render(request, "blog.html")
+
